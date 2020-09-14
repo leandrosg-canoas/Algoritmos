684070b9847057980454bd3cda05c3cabbbad25a
LLLEITE <leandro.lima.leite@hotmail.com>
2020-09-14 19:36:15 -0300

    no message

---
+++ A1 - Variaveis/Exemplos/Atribuicao_Composta.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    
+    int soma = 1;
+    int numero;
+
+    Console.WriteLine ("Digite um valor: ");
+    
+    numero = int.Parse( Console.ReadLine() );
+    
+    soma += numero;
+
+    Console.WriteLine("Soma: " + soma);
+
+    c = a * b;
+  }
+}
