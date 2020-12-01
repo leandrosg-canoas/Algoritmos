784095a864af75e41e063d460bcb0a9ea7244465
LLLEITE <leandro.lima.leite@hotmail.com>
2020-12-01 14:07:04 -0300

    Mais exemplos Lista 7

---
+++ A7 - POO/Exemplos/Enumerators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Um enum pode ser criado tanto fora quanto dentro de uma classe
+// Isso só muda a acessibilidade deste enum para outras classes
+// Passe o mouse sobre algum dos dias criados e veja o valor anexado a eles
+enum Day
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+namespace A7_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day currentDay = Day.Monday;
+
+            Console.WriteLine(currentDay);
+
+            Console.ReadKey();
+
+            // Mais informações aprofundadas
+            //https://www.devmedia.com.br/trabalhando-com-enums-em-csharp/26059
+        }
+    }
+};
---
+++ A7 - POO/Exemplos/Números Aleatórios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum Day
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+namespace A7_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = int.Parse(Console.ReadLine());
+
+            Day currentDay = Day.Sunday
+
+            Console.Write("Today is " + Convert.ToString(currentDay));
+
+            // Demais informações sobre como usar Enums
+            // 
+        }
+    }
+};
