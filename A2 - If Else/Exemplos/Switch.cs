3712952214c422b75f209daddaf58b70a6d44776
LLLEITE <leandro.lima.leite@hotmail.com>
2020-12-11 19:04:14 -0300

    Switch - Aula 2

---
+++ A2 - If Else/Exemplos/Switch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menu com Switch
+            Console.WriteLine("Menu de Switch");
+            Console.WriteLine("[1] Escolha A");
+            Console.WriteLine("[2] Escolha B" + "\n");
+
+            Console.WriteLine("Digite uma opção: ");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Escolheu A");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Escolheu B");
+                    break;
+
+                default: Console.WriteLine("Opção Inválida");
+                    break;
+            }
+        }
+    }
+};
