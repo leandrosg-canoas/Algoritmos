eadd437b533f0f39db0512a250f95c134d29b1aa
LLLEITE <leandro.lima.leite@hotmail.com>
2020-12-11 19:29:33 -0300

    Correação do Switch

--- A2 - If Else/Exemplos/Switch.cs
+++ A2 - If Else/Exemplos/Switch.cs
@@ -10,9 +10,30 @@ namespace A7_Exemplo
     {
         static void Main(string[] args)
         {
-            
+            // Menu com Switch
+            Console.WriteLine("Menu de Switch");
+            Console.WriteLine("[1] Escolha A");
+            Console.WriteLine("[2] Escolha B" + "\n");
 
-            Console.ReadLine();
+            // \n - Quebra a linha, coloca um espaço a mais
+
+            Console.Write("Digite uma opção: ");
+            option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Escolheu A");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Escolheu B");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Inválida");
+                    break;
+            }
         }
     }
 };
