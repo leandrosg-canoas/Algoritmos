504fc0a92287d363dc396aab688d2b2a93c5c4f5
LLLEITE <leandro.lima.leite@hotmail.com>
2020-12-11 19:25:40 -0300

    Menu com while

--- A2 - If Else/Exemplos/Switch.cs
+++ A2 - If Else/Exemplos/Switch.cs
@@ -10,29 +10,9 @@ namespace A7_Exemplo
     {
         static void Main(string[] args)
         {
-            // Menu com Switch
-            Console.WriteLine("Menu de Switch");
-            Console.WriteLine("[1] Escolha A");
-            Console.WriteLine("[2] Escolha B" + "\n");
+            
 
-            // \n - Quebra a linha, coloca um espaço a mais
-
-            Console.WriteLine("Digite uma opção: ");
-            int option = int.Parse(Console.ReadLine());
-
-            switch (option)
-            {
-                case 1:
-                    Console.WriteLine("Escolheu A");
-                    break;
-
-                case 2:
-                    Console.WriteLine("Escolheu B");
-                    break;
-
-                default: Console.WriteLine("Opção Inválida");
-                    break;
-            }
+            Console.ReadLine();
         }
     }
 };
---
+++ A3 - For While/Exemplos/MenuComWhile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A7_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 0;
+
+            while (option != -1)
+            {
+                // Menu com Switch
+                Console.WriteLine("Menu de Switch");
+                Console.WriteLine("[1] Escolha A");
+                Console.WriteLine("[2] Escolha B");
+                Console.WriteLine("[-1] Sair" + "\n");
+
+                // \n - Quebra a linha, coloca um espaço a mais
+
+                Console.Write("Digite uma opção: ");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Escolheu A \n");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Escolheu B \n");
+                        break;
+
+                    case -1:
+                        Console.WriteLine("Até mais \n");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção Inválida \n");
+                        break;
+                }
+            }
+        }
+    }
+};
